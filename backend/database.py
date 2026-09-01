79615519d9b457198b2df4e3bac2296425e53924
Maxim <barulinmaxim.it@gmail.com>
2026-09-01 17:57:42 +0300

    Додав Railway Volume для постійного зберігання бази даних

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATEBASE_URL = "sqlite:///./peekflix.db"
+if os.getenv("RAILWAY_ENVIRONMENT"):
+    DATEBASE_URL = "sqlite:////data/peekflix.db"
+else:
+    DATEBASE_URL = "sqlite:///./peekflix.db"
 
 engine = create_engine(DATEBASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
